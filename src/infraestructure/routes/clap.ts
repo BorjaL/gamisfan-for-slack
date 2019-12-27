ba28d3d75c7d5f440527c5d0ba05aab0d5c2ab7d
BorjaL <borja.fpillado@gmail.com>
2019-12-27 22:10:52 +0100

    Start using slack middleware for the creation clap query

--- src/infraestructure/routes/clap.ts
+++ src/infraestructure/routes/clap.ts
@@ -1,9 +1,10 @@
 import * as Router from "koa-router";
 import { ClapController } from "../controller/ClapController";
+import { slackMiddleware } from "./middleware/slackMiddleware";
 
 const router = new Router({ prefix: "/clap" });
 const clapController: ClapController = new ClapController();
 
-router.post("/", clapController.createClap.bind(clapController));
+router.post("/", slackMiddleware.verificate, clapController.createClap.bind(clapController));
 
 export default router;
